4e55c81fec2aa022ad05eb75f13beeaa5d334450
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-12-17 13:50:38 +0530

    added product detalis

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Welcome from "./Pages/welcome";
 import Products from "./Pages/Products";
+import ProductDetail from './Pages/ProductDetail';
 import MainHeader from "./Components/MainHeader";
 function App() {
     return (
       <div>
-        <MainHeader/>
+        <MainHeader />
         <main>
           <Route path="/welcome">
             <Welcome />
           </Route>
           <Route path="/Products">
             <Products />
+          </Route>
+          <Route path="/Product/:productId">
+            <ProductDetail />
           </Route>
         </main>
       </div>
--- src/Components/MainHeader.js
+++ src/Components/MainHeader.js
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import classes from './MainHeader.module.css';
+
+
 const MainHeader = () => {
-    return(
-        <header>
-            <nav>
-                <ul>
-                    <li> 
-                        <Link to="/welcome">welcome</Link>
-                    </li>
-                    <li> 
-                        <Link to="/Products">Products</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+    return (
+      <header classNmae={classes.header}>
+        <nav>
+          <ul>
+            <li>
+              <NavLink activeClassName={classes.active} to="/welcome">
+                welcome
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName={classes.active} to="/Products">
+                Products
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
 };
 
